15e9470bb90d9ee6d0c28b6cdd3945e8952aa9d1
Eric Yin <eric2323223@gmail.com>
2012-05-29 23:37:49 +0800

    html parser

--- src/main/com/sybase/supqa/tenacious/RftHtmlLogParser.java
+++ src/main/com/sybase/supqa/tenacious/RftHtmlLogParser.java
@@ -1,10 +1,43 @@
 package com.sybase.supqa.tenacious;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 public class RftHtmlLogParser {
+	private Document document;
 
 	public RftHtmlLogParser(String logFileName) {
-		// TODO Auto-generated constructor stub
+		try {
+			document = Jsoup.parse(new File(logFileName), "UTF-8");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
-	
+
+	//25-May-2012 06:04:06.875 PM
+	//yyyy年MM月dd日 HH时mm分ss秒
+	public Date parseStartTime() {
+		Elements elements = document.getElementsByClass("time");
+		String dateString = elements.get(0).text();
+		DateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSS a", Locale.ENGLISH);
+		try {
+			return format.parse(dateString);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RuntimeException("Failed to get date");
+		}
+	}
 
 }
--- src/test/com/sybase/supqa/tenacious/RftHtmlLogParserTest.java
+++ src/test/com/sybase/supqa/tenacious/RftHtmlLogParserTest.java
@@ -1,5 +1,8 @@
 package com.sybase.supqa.tenacious;
 
+import static org.junit.Assert.*;
+
+import java.io.File;
 import java.util.Date;
 
 import org.junit.Before;
@@ -9,14 +12,17 @@ import org.junit.Test;
 public class RftHtmlLogParserTest {
 	
 	private RftHtmlLogParser parser;
+	private TenaciousConfig cfg;
 	
 	@Before public void setup(){
-		parser = new RftHtmlLogParser("");
+		cfg = new TenaciousConfig();
+		String testLogFile = cfg.getTenaciousRootPath()+"\\src\\test\\fixture\\rational_ft_logframe.html";
+		parser = new RftHtmlLogParser(testLogFile);
 	}
 
 	@Test public void shouldGetStartTime(){
 		Date date = parser.parseStartTime();
-		assertEquals();
+		assertEquals(new Date(), date);
 	}
 
 }
